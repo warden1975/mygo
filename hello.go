228b2557a59634e5f4f254c5389d2f18ce9a5e01
Warren Gutierrez <warrengutierrez@Warrens-iMac.local>
2013-06-05 03:38:50 +0800

    added custom package newmath Sqrt()

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/warden1975/newmath"
+)
 
 func main() {
-	fmt.Printf("Welcome to going bananas!\n")
+	fmt.Printf("Welcome to going bananas! Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
